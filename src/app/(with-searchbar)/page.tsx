de2b90a65e58636440cebdead0a054c8b90ab8ee
ymj0828 <96277798+ymj0828@users.noreply.github.com>
2024-09-26 23:22:50 +0900

    feat: 페이지 데이터 페칭 설정

--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -1,4 +1,4 @@
-import movies from '@/mock/dummy.json';
+import { MovieData } from '@/types';
 
 import style from './page.module.css';
 import classNames from 'classnames/bind';
@@ -7,24 +7,52 @@ import MovieItem from '@/components/movie-item';
 
 const cx = classNames.bind(style);
 
+async function AllMovies() {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/movie`, {
+    cache: 'force-cache',
+  });
+  if (!response.ok) {
+    return <div>오류가 발생했습니다...</div>;
+  }
+
+  const allMovies: MovieData[] = await response.json();
+  return (
+    <div className={cx('all_container')}>
+      {allMovies.map((movie) => (
+        <MovieItem key={`all-${movie.id}`} {...movie} />
+      ))}
+    </div>
+  );
+}
+
+async function RecoMovies() {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/movie/random`, {
+    next: { revalidate: 60 },
+  });
+  if (!response.ok) {
+    return <div>오류가 발생했습니다...</div>;
+  }
+
+  const recoMovies: MovieData[] = await response.json();
+  return (
+    <div className={cx('reco_container')}>
+      {recoMovies.map((movie) => (
+        <MovieItem key={`reco-${movie.id}`} {...movie} />
+      ))}
+    </div>
+  );
+}
+
 export default function Home() {
   return (
     <div className={cx('container')}>
       <section>
         <h3>지금 가장 추천하는 영화</h3>
-        <div className={cx('reco_container')}>
-          {movies.slice(0, 3).map((movie) => (
-            <MovieItem key={`reco-${movie.id}`} {...movie} />
-          ))}
-        </div>
+        <RecoMovies />
       </section>
       <section>
         <h3>등록된 모든 영화</h3>
-        <div className={cx('all_container')}>
-          {movies.map((movie) => (
-            <MovieItem key={`all-${movie.id}`} {...movie} />
-          ))}
-        </div>
+        <AllMovies />
       </section>
     </div>
   );
