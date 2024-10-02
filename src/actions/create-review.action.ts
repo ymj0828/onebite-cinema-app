75ff568a27429c09538ff4c918c0a9381e20742c
ymj0828 <96277798+ymj0828@users.noreply.github.com>
2024-10-02 17:04:30 +0900

    feat: useActionState를 사용하여 리뷰 추가 기능 보완

--- src/actions/create-review.action.ts
+++ src/actions/create-review.action.ts
@@ -1,14 +1,17 @@
 'use server';
 
-export async function createReviewAction(formData: FormData) {
+import { revalidateTag } from 'next/cache';
+
+export async function createReviewAction(_: any, formData: FormData) {
   const movieId = formData.get('movieId')?.toString();
   const content = formData.get('content')?.toString();
   const author = formData.get('author')?.toString();
 
-  console.log(movieId, content, author);
-
   if (!content || !author || !movieId) {
-    return;
+    return {
+      status: false,
+      error: '리뷰 내용과 작성자를 입력해주세요',
+    };
   }
 
   try {
@@ -17,9 +20,20 @@ export async function createReviewAction(formData: FormData) {
       body: JSON.stringify({ movieId, content, author }),
     });
 
-    console.log(response.status);
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+
+    revalidateTag(`review-${movieId}`);
+
+    return {
+      status: true,
+      error: '',
+    };
   } catch (err) {
-    console.error(err);
-    return;
+    return {
+      status: false,
+      error: `리뷰 저장에 실패했습니다 : ${err}`,
+    };
   }
 }
